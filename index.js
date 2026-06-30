300b8588582d3e3daff40c0b78f2b1aca76e1e73
Arton18 <arton.melo@aluno.uepb.edu.br>
2026-06-30 01:23:58 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const pool = require('./bd')
 
 const app = express();
 const usuariosRoutes=require('./routes/usuarios');
+const treinosRoutes= require('./routes/treinos');
+const exerciciosRoutes=require('./routes/exercicios');
 app.use(express.json());
 app.use(express.static('front'))
 app.use('/api',usuariosRoutes)
+app.use('/api',treinosRoutes)
+app.use('/api',exerciciosRoutes)
 
 app.listen(3000,()=>{
   console.log('va para para ver o funcionamento http://localhost:3000/api/usuarios')});
