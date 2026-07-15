c931036103390fa96824bb5b800f9e37e8722dd0
Arton18 <arton.melo@aluno.uepb.edu.br>
2026-07-15 15:46:36 -0300

    Add files via upload
    atualização do index

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const app = express();
 const usuariosRoutes=require('./routes/usuarios');
 const treinosRoutes= require('./routes/treinos');
 const exerciciosRoutes=require('./routes/exercicios');
+const cronogramaRoutes=require('./routes/cronograma');
+
 app.use(express.json());
 app.use(express.static('front'))
 app.use('/api',usuariosRoutes)
 app.use('/api',treinosRoutes)
 app.use('/api',exerciciosRoutes)
+app.use('/api',cronogramaRoutes)
+
 
 app.listen(3000,()=>{
   console.log('va para para ver o funcionamento http://localhost:3000/api/usuarios')});
